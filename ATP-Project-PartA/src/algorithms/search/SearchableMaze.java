de4b181eadeca7c554e7dd671722afaee057d5f5
OfekKah <127878076+OfekKah@users.noreply.github.com>
2024-06-16 17:57:38 +0300

    Update SearchableMaze.java

--- ATP-Project-PartA/src/algorithms/search/SearchableMaze.java
+++ ATP-Project-PartA/src/algorithms/search/SearchableMaze.java
@@ -4,30 +4,39 @@ import algorithms.mazeGenerators.Maze;
 import algorithms.mazeGenerators.Position;
 import java.util.ArrayList;
 
+// SearchableMaze class implements the ISearchable interface for a maze
 public class SearchableMaze implements ISearchable {
-    private final Maze maze;
+    private final Maze maze; // The maze to be searched
 
+    // Constructor initializing the maze
     public SearchableMaze(Maze maze) throws IllegalArgumentException {
         if (maze == null)
-            throw new IllegalArgumentException("null maze");
+            throw new IllegalArgumentException("null maze"); // Throw exception if maze is null
         this.maze = maze;
     }
 
+    // Get the start state of the maze
     @Override
     public AState getStartState() {
-        return new MazeState(null, maze.getStartPosition(), 0);
-    }
-    @Override
-    public AState getGoalState() {
-        return new MazeState(null, maze.getGoalPosition(), 0);
+        return new MazeState(null, maze.getStartPosition(), 0); // Return start position with zero cost
     }
 
+    // Get the goal state of the maze
+    @Override
+    public AState getGoalState() {
+        return new MazeState(null, maze.getGoalPosition(), 0); // Return goal position with zero cost
+    }
+
+    // Get all possible states (neighbors) from the given state
     @Override
     public ArrayList<AState> getAllPossibleStates(AState state) {
         ArrayList<AState> possibleStates = new ArrayList<>();
         if (state == null)
-            return possibleStates;
-        Position currentPosition = (Position) state.getCurrentState();
+            return possibleStates; // Return empty list if the state is null
+
+        Position currentPosition = (Position) state.getCurrentState(); // Get current position
+
+        // Check all possible moves and add valid ones to the possible states list
 
         // Up
         if (maze.isValidPosition(currentPosition.getUpPosition()) && !maze.IsWall(currentPosition.getUpPosition()))
@@ -45,24 +54,26 @@ public class SearchableMaze implements ISearchable {
         if (maze.isValidPosition(currentPosition.getLeftPosition()) && !maze.IsWall(currentPosition.getLeftPosition()))
             possibleStates.add(new MazeState(state, currentPosition.getLeftPosition(), state.getCost() + 10));
 
-
         // Right-Up
-        if (maze.isValidPosition(currentPosition.getRightUpPosition()) && !maze.IsWall(currentPosition.getRightUpPosition()) && (!maze.IsWall(currentPosition.getUpPosition()) || !maze.IsWall(currentPosition.getRightPosition())))
-            possibleStates.add(new MazeState(state, currentPosition.getRightPosition(), state.getCost() + 15));
+        if (maze.isValidPosition(currentPosition.getRightUpPosition()) && !maze.IsWall(currentPosition.getRightUpPosition()) &&
+                (!maze.IsWall(currentPosition.getUpPosition()) || !maze.IsWall(currentPosition.getRightPosition())))
+            possibleStates.add(new MazeState(state, currentPosition.getRightUpPosition(), state.getCost() + 15));
 
         // Right-Down
-        if (maze.isValidPosition(currentPosition.getRightDownPosition()) && !maze.IsWall(currentPosition.getRightDownPosition()) && (!maze.IsWall(currentPosition.getDownPosition()) || !maze.IsWall(currentPosition.getRightPosition())))
+        if (maze.isValidPosition(currentPosition.getRightDownPosition()) && !maze.IsWall(currentPosition.getRightDownPosition()) &&
+                (!maze.IsWall(currentPosition.getDownPosition()) || !maze.IsWall(currentPosition.getRightPosition())))
             possibleStates.add(new MazeState(state, currentPosition.getRightDownPosition(), state.getCost() + 15));
 
         // Left-Up
-        if (maze.isValidPosition(currentPosition.getLeftUpPosition()) && !maze.IsWall(currentPosition.getLeftUpPosition()) && (!maze.IsWall(currentPosition.getUpPosition()) || !maze.IsWall(currentPosition.getLeftPosition())))
+        if (maze.isValidPosition(currentPosition.getLeftUpPosition()) && !maze.IsWall(currentPosition.getLeftUpPosition()) &&
+                (!maze.IsWall(currentPosition.getUpPosition()) || !maze.IsWall(currentPosition.getLeftPosition())))
             possibleStates.add(new MazeState(state, currentPosition.getLeftUpPosition(), state.getCost() + 15));
 
         // Left-Down
-        if (maze.isValidPosition(currentPosition.getLeftDownPosition()) && !maze.IsWall(currentPosition.getLeftDownPosition()) && (!maze.IsWall(currentPosition.getDownPosition()) || !maze.IsWall(currentPosition.getLeftPosition())))
+        if (maze.isValidPosition(currentPosition.getLeftDownPosition()) && !maze.IsWall(currentPosition.getLeftDownPosition()) &&
+                (!maze.IsWall(currentPosition.getDownPosition()) || !maze.IsWall(currentPosition.getLeftPosition())))
             possibleStates.add(new MazeState(state, currentPosition.getLeftDownPosition(), state.getCost() + 15));
 
-
-        return possibleStates;
+        return possibleStates; // Return all valid possible states
     }
 }
