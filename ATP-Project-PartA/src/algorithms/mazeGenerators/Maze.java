e8e5ec1c610a64c9305a1141698ac7d170d2503a
kattomer <119042639+kattomer@users.noreply.github.com>
2024-06-16 17:53:21 +0300

    Update Maze.java

--- ATP-Project-PartA/src/algorithms/mazeGenerators/Maze.java
+++ ATP-Project-PartA/src/algorithms/mazeGenerators/Maze.java
@@ -1,32 +1,41 @@
 package algorithms.mazeGenerators;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Random;
 
 public class Maze {
-    private int[][] maze;
-    private Position start;
-    private Position goal;
+    private int[][] maze; // 2D array to represent the maze
+    private Position start; // Starting position of the maze
+    private Position goal; // Goal position of the maze
+
+    // Constructor to initialize the maze with given dimensions
     public Maze(int row, int col) {
         maze = new int[row][col];
-        this.start = new Position(0,0);
-        this.goal = new Position(row-1, col-1);
+        this.start = new Position(0, 0); // Default start position
+        this.goal = new Position(row - 1, col - 1); // Default goal position
     }
 
+    // Get the number of columns in the maze
     private int getMazeCols() {
         return maze[0].length;
     }
+
+    // Get the number of rows in the maze
     private int getMazeRows() {
         return maze.length;
     }
-    public void addEmptyCell(int row, int col){
+
+    // Add an empty cell (path) at the given row and column
+    public void addEmptyCell(int row, int col) {
         maze[row][col] = 0;
     }
-    public void addWall(int row, int col){
+
+    // Add a wall at the given row and column
+    public void addWall(int row, int col) {
         maze[row][col] = 1;
     }
+
+    // Check if the given position is a wall
     public boolean IsWall(Position position) {
         if (isValidPosition(position)) {
             return maze[position.getRowIndex()][position.getColumnIndex()] == 1;
@@ -34,46 +43,54 @@ public class Maze {
         return false;
     }
 
-    public Position getStartPosition(){
+    // Get the start position
+    public Position getStartPosition() {
         return start;
     }
-    public Position getGoalPosition(){
+
+    // Get the goal position
+    public Position getGoalPosition() {
         return goal;
     }
 
+    // Set a cell as a path (transition) at the given position
     public void SetTransition(Position position) {
-        if (position.getRowIndex() >= 0 && position.getColumnIndex() >= 0)
+        if (position.getRowIndex() >= 0 && position.getColumnIndex() >= 0) {
             this.maze[position.getRowIndex()][position.getColumnIndex()] = 0;
+        }
     }
 
+    // Check if the given position is valid within the maze boundaries
     public boolean isValidPosition(Position position) {
-        if (position == null) { return false;}
+        if (position == null) {
+            return false;
+        }
 
         int row = position.getRowIndex();
         int col = position.getColumnIndex();
-        return row >= 0 && row < this.getMazeRows() &&
-                col >= 0 && col < this.getMazeCols();
+        return row >= 0 && row < this.getMazeRows() && col >= 0 && col < this.getMazeCols();
     }
 
+    // Get a list of unvisited neighbour positions two steps away
     public ArrayList<Position> doubleSteps(Position position) {
         ArrayList<Position> wallsList = new ArrayList<>();
         if (position != null) {
             Position up = position.getUpPosition().getUpPosition();
-            if (this.isValidPosition(up) && IsWall(up))
-                wallsList.add(up);
+            if (this.isValidPosition(up) && IsWall(up)) wallsList.add(up);
+
             Position right = position.getRightPosition().getRightPosition();
-            if (this.isValidPosition(right) && IsWall(right))
-                wallsList.add(right);
+            if (this.isValidPosition(right) && IsWall(right)) wallsList.add(right);
+
             Position down = position.getDownPosition().getDownPosition();
-            if (this.isValidPosition(down) && IsWall(down))
-                wallsList.add(down);
+            if (this.isValidPosition(down) && IsWall(down)) wallsList.add(down);
+
             Position left = position.getLeftPosition().getLeftPosition();
-            if (this.isValidPosition(left) && IsWall(left))
-                wallsList.add(left);
+            if (this.isValidPosition(left) && IsWall(left)) wallsList.add(left);
         }
         return wallsList;
     }
 
+    // Connect two neighbouring positions by removing the wall between them
     public void connectNeighbours(Position currentPosition, Position neighbour) throws IllegalArgumentException {
         if (!this.isValidPosition(currentPosition) || !this.isValidPosition(neighbour)) {
             throw new IllegalArgumentException();
@@ -89,50 +106,53 @@ public class Maze {
         }
     }
 
-    public static final String RED = "\033[0;31m";      // RED
-    public static final String GREEN = "\033[0;32m";    // GREEN
-    public static final String RESET = "\033[0m";       // Text Reset
+    // Color codes for printing the maze
+    public static final String RED = "\033[0;31m"; // RED
+    public static final String GREEN = "\033[0;32m"; // GREEN
+    public static final String RESET = "\033[0m"; // Text Reset
 
+    // Print the maze with start and goal positions highlighted
     public void print() {
         for (int i = 0; i < this.getMazeRows(); i++) {
             System.out.print("{");
             for (int j = 0; j < this.getMazeCols(); j++) {
-                if (this.start.equals(new Position(i, j)))
+                if (this.start.equals(new Position(i, j))) {
                     System.out.print(RED + " S" + RESET);
-                else if (this.goal.equals(new Position(i, j)))
+                } else if (this.goal.equals(new Position(i, j))) {
                     System.out.print(GREEN + " E" + RESET);
-                else
+                } else {
                     System.out.print(" " + this.maze[i][j]);
+                }
             }
             System.out.println(" }");
         }
     }
 
+    // Set a random valid goal position on the maze boundary
     public void setGoalPosition() {
         Random rnd = new Random();
         ArrayList<Position> goalPositionOptions = new ArrayList<>();
         int columnsSize = this.getMazeCols();
         int rowSize = this.getMazeRows();
 
+        // Check boundary cells
         for (int i = 0; i < columnsSize; i++) {
-            if (this.maze[0][i] == 0)
-                goalPositionOptions.add(new Position(0, i));
-            if (this.maze[rowSize - 1][i] == 0)
-                goalPositionOptions.add(new Position(rowSize - 1, i));
+            if (this.maze[0][i] == 0) goalPositionOptions.add(new Position(0, i));
+            if (this.maze[rowSize - 1][i] == 0) goalPositionOptions.add(new Position(rowSize - 1, i));
         }
 
         for (int i = 0; i < rowSize; i++) {
-            if (this.maze[i][0] == 0)
-                goalPositionOptions.add(new Position(i, 0));
-            if (this.maze[i][columnsSize - 1] == 0)
-                goalPositionOptions.add(new Position(i, columnsSize - 1));
+            if (this.maze[i][0] == 0) goalPositionOptions.add(new Position(i, 0));
+            if (this.maze[i][columnsSize - 1] == 0) goalPositionOptions.add(new Position(i, columnsSize - 1));
         }
-        if (goalPositionOptions.size() <= 1)
-            throw new RuntimeException();
 
-        do
-            this.goal = (goalPositionOptions.get(rnd.nextInt(goalPositionOptions.size())));
-        while (this.getStartPosition().equals(this.getGoalPosition()));
+        if (goalPositionOptions.size() <= 1) {
+            throw new RuntimeException("No valid goal position found.");
+        }
+
+        // Select a random goal position that is different from the start position
+        do {
+            this.goal = goalPositionOptions.get(rnd.nextInt(goalPositionOptions.size()));
+        } while (this.getStartPosition().equals(this.getGoalPosition()));
     }
 }
-
